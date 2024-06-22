c188859dd2659014b7e90ec61cf48958a944c7d7
Xudong ZHAO <xudong.zhao@protonmail.com>
2024-06-22 12:32:34 +0000

    [LeetCode Sync] Runtime - 9 ms (58.36%), Memory - 2.9 MB (90.03%)

---
+++ leetCode-solutions/0121-best-time-to-buy-and-sell-stock/solution.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        let mut max_profit:i32 = 0;
+        let mut min_price:i32 = i32::MAX;
+
+        for &price in prices.iter() {
+            if price < min_price {
+                min_price = price;
+            } else {
+                let profit = price - min_price;
+                if profit > max_profit {
+                    max_profit = profit;
+                }
+            }
+        }
+        return max_profit;
+    }
+}
